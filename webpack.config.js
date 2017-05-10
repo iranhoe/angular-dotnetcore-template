de132f4340920c80ca7a39a7ec0ad86c19ae112e
iranhoe <iranhoe@live.com.mx>
2017-05-09 22:10:45 -0500

    removing backup files

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './Scripts/main.js',
+    entry: './Scripts/main.ts',
     output: {
-        path: path.resolve(__dirname, './wwwroot/dist'),
+        path: path.resolve(__dirname, './wwwroot/js/dist'),
         filename: 'app.bundle.js'
     },
     module: {
